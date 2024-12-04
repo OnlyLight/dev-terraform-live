a10bc4b81b3035b0cfaa74d2c0613d8245e6cdf3
duytran <duyquangtran@kms-technology.com>
2024-12-04 00:34:57 -0600

    Change environment

--- staging/provider.tf
+++ staging/provider.tf
@@ -17,7 +17,7 @@ terraform {
   #   # manually, uncomment and fill in the config below.
 
   #   # bucket         = "<YOUR S3 BUCKET>"
-  #   # key            = "prod/terraform.tfstate"
+  #   # key            = "staging/terraform.tfstate"
   #   # region         = "us-east-2"
   #   # dynamodb_table = "<YOUR DYNAMODB TABLE>"
   #   # encrypt        = true
